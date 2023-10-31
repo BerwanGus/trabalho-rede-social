a8420afe856675f5276eacf670eeed61a07ee64e
Andre Note Positivo <andre-positivo@gmail.com>
2023-10-30 21:43:55 -0300

    Ecoville - Segunda 30/10

--- projetoTumbrl/tumbrl/routes.py
+++ projetoTumbrl/tumbrl/routes.py
@@ -1,13 +1,47 @@
 # Aqui vão as rotas e os links
 from tumbrl import app
-from flask import render_template, url_for
-from flask_login import login_required
+from flask import render_template, url_for, redirect
+from flask_login import login_required, login_user
 from tumbrl.models import load_user
+from tumbrl.forms import FormLogin, FormCreateNewAccount
+from tumbrl import bcrypt
+from models import User
+from tumbrl import database
+
 
 # @app.route('/home')
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def homepage():
-    return render_template('home.html', textinho='TOP')
+    _formLogin = FormLogin()
+    return render_template('home.html', textinho='TOP', form=_formLogin)
+
+
+@app.route('/new', methods=['POST', 'GET'])
+def createAccount():
+    _formCreateNewAccount = FormCreateNewAccount()
+
+    if _formCreateNewAccount.validate_on_submit():
+        password = _formCreateNewAccount.password.data
+        password_cr = bcrypt.generate_password_hash(password)
+        # print(password)
+        # print(password_cr)
+
+        newUser = User(
+            username=_formCreateNewAccount.usarname.data,
+            email=_formCreateNewAccount.email.data,
+            password=password
+        )
+
+        database.session.add(newUser)
+        database.session.commit()
+
+        # Desafio
+        # Fazer Login e Mandar para a pagina de perfil dele
+
+        login_user(newUser, remember=True)
+        return redirect(url_for('profile.html', user_id=newUser.id))
+
+    return render_template('new.html', form=_formCreateNewAccount)
 
 
 @app.route('/perry')
@@ -24,4 +58,3 @@ def teste():
 @login_required
 def profile(user_id):  # , batata):
     return render_template('profile.html', user=user_id)  # , mesa=batata)
-
